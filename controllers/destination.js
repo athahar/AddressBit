f9c7d86b3ed99f1b6c1b689761300cacf795ae1b
Athahar <athahar@gmail.com>
2014-02-26 00:35:36 -0800

    Using Pearson API for travel top 10

--- controllers/destination.js
+++ controllers/destination.js
@@ -1,24 +1,74 @@
 'use strict';
 
 
-var DestinationModel = require('../models/destination');
+var DestinationModel = require('../models/destination'),
+    http = require('http'),
+    https = require('https');
 
 
 module.exports = function (app) {
 
-    var model = new DestinationModel();
-
 
     app.get('/destination', function (req, res) {
-        
-        res.format({
-            json: function () {
-                res.json(model);
-            },
-            html: function () {
-                res.render('destination', model);
-            }
+
+
+        var opts = {
+            host: "api.pearson.com",
+            path: '/v2/travel/topten?limit=10&offset=0',
+            method: "GET"
+        },
+        request = http.request(opts, function(resp) {
+            var data = "";
+                resp.setEncoding('utf8');
+
+            resp.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            resp.on('end', function () {
+                //console.dir(data);
+                var model = JSON.parse(data)
+                console.dir(model);                
+                res.render('destination',model);
+            });
         });
+    request.end();
+        
+    });
+
+    app.get('/v2/travel/topten/:id', function (req, res) {
+
+        var opts = {
+            host: "api.pearson.com",
+            path: req.url,
+            method: "GET"
+        },
+        request = http.request(opts, function(resp) {
+            var data = "";
+                resp.setEncoding('utf8');
+
+            resp.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            resp.on('end', function (err) {
+
+                if(err){
+                     console.log(error);
+                     return;
+                }
+                //console.dir(data);
+                try{
+                     var model = JSON.parse(data);
+                    // console.dir(model);                
+                    res.render('destinationPath',model);
+                }catch(err){
+                    console.log(err);
+                }
+            });
+        });
+        request.end();
+        
     });
 
 };
--- models/destination.js
+++ models/destination.js
@@ -1,8 +1,28 @@
 'use strict';
 
+var PearsonApis = require("pearson-api-sdk");
+var request = require ('request');
 
-module.exports = function DestinationModel() {
-    return {
-        name: 'addressbit2-0'
-    };
+module.exports =  function () {
+
+ 	// Example request with no apikey (sandbox) and no proxy
+
+    var travelApi = PearsonApis.travel();
+    var topten = travelApi.topten;
+
+    var requestUrl = topten.getSearchUrl(); // This sets up the parameters and search terms described 
+
+
+    console.log('requestUrl: ' + requestUrl);    
+
+	 request(requestUrl, function (error, response, model) {
+          if (!error && response.statusCode == 200) {            
+          //  console.log(model);
+            return model
+        } else {
+            console.log(error);
+            return;
+          }
+        });
+
 };
